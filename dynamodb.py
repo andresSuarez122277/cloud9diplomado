0f3cd046f84f7555a1d29bc9c109bffb2ea7e97e
EC2 Default User <ec2-user@ip-172-31-1-56.ec2.internal>
2023-11-21 02:16:22 +0000

    func listar tablas

---
+++ dynamodb.py
@@ -0,0 +1,12 @@
+import boto3
+
+#print("SI")
+
+#crear funcion  para listar tablas de dynamodb
+def listar_tablas():
+    dynamodb = boto3.resource('dynamodb')
+    for table in dynamodb.tables.all():
+        print(table.name)
+
+listar_tablas()
+
